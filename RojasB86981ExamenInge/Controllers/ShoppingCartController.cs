e0eff9e952bd1f20058beb0aad641c750b73b60d
MauricioRojas17 <maurojseg17@gmail.com>
2021-12-03 05:08:07 -0600

    HU6:  Implementación de un carrito de compras para los productos del menú

---
+++ RojasB86981ExamenInge/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RojasB86981ExamenInge.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
